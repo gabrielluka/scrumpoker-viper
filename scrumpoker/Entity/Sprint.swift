410bcc93f2981d6aefd165305e8e61ba7b95b851
Gabriel Lucas <gabriel.santos-4@syngenta.com>
2021-06-20 18:00:13 -0300

    init project

---
+++ scrumpoker/Entity/Sprint.swift
@@ -0,0 +1,15 @@
+//
+//  Sprint.swift
+//  scrumpoker
+//
+//  Created by Gabriel Lucas Santos on 20/06/21.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct Sprint: Codable {
+    let id: Int
+    let nome: String
+    let link: String
+}
